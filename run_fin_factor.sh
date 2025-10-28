0b4c952dfffb60e9f6da759ae262a2d3bae542c5
wjh720 <17448305+wjh720@users.noreply.github.com>
2025-10-28 22:43:48 +0800

    Update run_fin_factor.sh

--- run_fin_factor.sh
+++ run_fin_factor.sh
@@ -3,4 +3,4 @@
 
  Foldername="1134_rdagent_fin_factor_aa"
  mkdir -p out_logs/${Foldername} &> /dev/null
- RICH_NO_COLOR=1 PYTHONUNBUFFERED=1 rdagent fin_factor --loop-n 1 >>out_logs/${Foldername}/rdagent_fin_quant.txt 2>&1
+ RICH_NO_COLOR=1 PYTHONUNBUFFERED=1 rdagent fin_factor --loop-n 1 >>out_logs/${Foldername}/rdagent_fin_factor.txt 2>&1
