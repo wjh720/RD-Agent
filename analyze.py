6543f29ab5fae1c7deff284c3d7614b101d06058
wjh720 <17448305+wjh720@users.noreply.github.com>
2025-10-29 17:32:26 +0800

    Create analyze.py

---
+++ analyze.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_pickle("log/2025-10-29_01-12-54-858255/__session__/14/0_direct_exp_gen")
+hypothesis, sota_exp = data.trace.get_sota_hypothesis_and_experiment()
+sota_result = sota_exp.result
+print("SOTA results:")
+print(sota_result)
+print("SOTA factors:")
+for prev_exp in sota_exp.based_experiments:
+    for sub_task in prev_exp.sub_tasks:
+        print(sub_task.factor_name)
+        print("..", sub_task.factor_formulation)
+
+for sub_task in sota_exp.sub_tasks:
+    print(sub_task.factor_name)
+    print("..", sub_task.factor_formulation)
