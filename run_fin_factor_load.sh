afb604e7dfda08f2f7207b492b784b20cdcd5ea4
wjh720 <17448305+wjh720@users.noreply.github.com>
2025-11-01 11:48:10 +0800

    Update run_fin_factor_load.sh

--- run_fin_factor_load.sh
+++ run_fin_factor_load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
    # Script to reproduce results
 
- Foldername="1142_rdagent_fin_factor_load_aa"
+ Foldername="1143_rdagent_fin_factor_load_aa"
  mkdir -p out_logs/${Foldername} &> /dev/null
  RICH_NO_COLOR=1 PYTHONUNBUFFERED=1 rdagent fin_factor --path log/2025-10-29_01-12-54-858255/__session__/20/3_feedback --loop-n 50 >>out_logs/${Foldername}/rdagent_fin_factor_load.txt 2>&1
